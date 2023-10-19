debcda47e923cf5131d1a2e540fe22995d6083e6
Aishwarya <aishwarya.verma28@gmail.com>
2023-10-19 10:37:02 +0530

    apis changed

--- src/components/bookmyplayer/StrategyModal.jsx
+++ src/components/bookmyplayer/StrategyModal.jsx
@@ -4,6 +4,23 @@ import "../styles/HelpModal.css";
 
 const StrategyModal = ({ onClose }) => {
 
+  const [formData, setFormData] = useState({
+    strategy_name: "",
+    strategy_desc: ""
+  });
+
+  const handleNameChange = (e) => {
+    setFormData({ ...formData, strategy_name: e.target.value });
+  };
+
+  const handleDescChange = (e) => {
+    setFormData({ ...formData, strategy_desc: e.target.value });
+  };
+
+  const handleSave = () => {
+    // Here, you can access formData and do whatever you want with it
+    console.log(formData);
+  };
   return (
     <>
       <div className="help-modal-container">
@@ -16,7 +33,8 @@ const StrategyModal = ({ onClose }) => {
           <div className="bmp-modal-form">
             <div className="bmp-add-fields">
             <label htmlFor="" className="common-fonts light-color">Strategy Name</label>
-                <input type="text" className="common-fonts common-input bmp-modal-input" />
+                <input type="text" className="common-fonts common-input bmp-modal-input"  value={formData.strategy_name}
+                  onChange={handleNameChange}/>
             </div>
             <div className="bmp-add-fields">
             <label htmlFor="" className="common-fonts light-color">Strategy Description</label>
