3088e0ed7b2335c0d0a5f6f55199bf8c52c439ad
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2023-11-22 15:52:57 +0530

    review

--- src/components/settings/LPSettingSidebar.jsx
+++ src/components/settings/LPSettingSidebar.jsx
@@ -323,6 +323,7 @@ const LPSettingSidebar = () => {
             </div>
           )}
 
+
           {(isPathAllowed("/lp/settings/sitePages/add") ||
             isPathAllowed("/lp/settings/sitePages/view")) && (
             <div
