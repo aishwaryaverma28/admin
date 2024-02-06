d9103535d72675c43c4a4628daa37c13e550c1c7
taher2552 <stardom.taher@gmail.com>
2024-02-06 17:46:43 +0530

    changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <section className='dashboard_view'>
-    <div className='dashboard_header'>
-
-    <div>
-        <select className='selectSec'>
+    <section>
+      <div className="dashboard_header">
+        <div>
+          <select className="selectSec">
             <option value="">Select Days</option>
             <option value="today">Today</option>
             <option value="yesterday">Yesterday</option>
@@ -17,22 +16,65 @@ const Dashboard = () => {
             <option value="last_twenty_eight_days">Last 28 days</option>
             <option value="last_thirty_days">Last 30 days</option>
             <option value="last_sixty_days">Last 60 days</option>
-        </select>
-    </div>
-    <div>
-    <select class="selectSec">
+          </select>
+        </div>
+        <div>
+          <select class="selectSec">
             <option value="">Select Site</option>
             <option value="bookmyplayer">bookmyplayer</option>
             <option value="leadplaner">leadplaner</option>
             <option value="firstcron">firstcron</option>
-           <option value="routplaner">routplaner</option>
-        </select>
-    </div>
+            <option value="routplaner">routplaner</option>
+          </select>
+        </div>
+      </div>
 
-    </div>
-      
+      <div className="dashboard_view">
+        <div className="dashboard_left">
+          <h2>OTP</h2>
+
+          <div className="dashboard_item">
+            <div className="dashboard_card">
+              <h3 className="common_fontscommon_fonts">Login</h3>
+              <p className="common_fonts">94</p>
+            </div>
+            <div className="dashboard_card">
+              <h3 className="font_new">Registration</h3>
+              <p className="common_fonts">36</p>
+            </div>
+          </div>
+
+          <h2>USERS</h2>
+
+          <div className="dashboard_item">
+            <div className="dashboard_card">
+              <h3 className="common_fontscommon_fonts">Academy</h3>
+              <p className="common_fonts">94</p>
+            </div>
+            <div className="dashboard_card">
+              <h3 className="font_new">Individuals</h3>
+              <p className="common_fonts">36</p>
+            </div>
+          </div>
+
+          <div className="dashboard_item">
+            <div className="dashboard_card">
+              <h3 className="common_fontscommon_fonts">Leads</h3>
+              <p className="common_fonts">70</p>
+            </div>
+            <div className="dashboard_card">
+              <h3 className="font_new">Subscriptions</h3>
+              <p className="common_fonts">34</p>
+            </div>
+          </div>
+
+          <div className="dashboard_item"></div>
+          <div className="dashboard_item"></div>
+          <div className="dashboard_item"></div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
