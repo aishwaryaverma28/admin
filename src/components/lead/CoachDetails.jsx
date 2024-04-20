b084bc99b30f67e9664ce9ddf17cf13097fe83af
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-04-20 12:49:14 +0530

    coach skills added

--- src/components/lead/CoachDetails.jsx
+++ src/components/lead/CoachDetails.jsx
@@ -27,7 +27,7 @@ const CoachDetails = React.forwardRef(({id, updateCheckState}, ref ) => {
         },
       })
       .then((response) => {
-        const sport = response?.data?.data[0]?.sport?.toLowerCase();
+        const sport = response?.data?.data[0]?.sport;
         setEditedItem(response?.data?.data[0]);
         setIsLoading(false);
         if (response?.data?.data[0]?.training_location) {
@@ -61,7 +61,7 @@ const CoachDetails = React.forwardRef(({id, updateCheckState}, ref ) => {
       [name]: value,
     });
     if (name === "sport") {
-      const sport = value.toLowerCase();
+      const sport = value;
       if (skills[sport]) {
         setUserSkills(skills[sport]);
       } else {
@@ -390,39 +390,39 @@ const CoachDetails = React.forwardRef(({id, updateCheckState}, ref ) => {
                         }
                         disabled={isDisabled} />
                       <datalist id="sports">
-                        <option value="archery"></option>
-                        <option value="arts"></option>
-                        <option value="athletics"></option>
-                        <option value="aerobics"></option>
-                        <option value="badminton"></option>
-                        <option value="basketball"></option>
-                        <option value="bodybuilding"></option>
-                        <option value="billiards"></option>
-                        <option value="boxing"></option>
-                        <option value="chess"></option>
-                        <option value="cricket"></option>
-                        <option value="fencing"></option>
-                        <option value="football"></option>
-                        <option value="golf"></option>
-                        <option value="gym"></option>
-                        <option value="hockey"></option>
-                        <option value="kabaddi"></option>
-                        <option value="karate"></option>
-                        <option value="kho-kho"></option>
-                        <option value="mma"></option>
-                        <option value="motorsports"></option>
-                        <option value="rugby"></option>
-                        <option value="shooting"></option>
-                        <option value="skating"></option>
-                        <option value="sports"></option>
-                        <option value="squash"></option>
-                        <option value="swimming"></option>
-                        <option value="table-tennis"></option>
-                        <option value="taekwondo"></option>
-                        <option value="tennis"></option>
-                        <option value="volleyball"></option>
-                        <option value="wrestling"></option>
-                        <option value="yoga"></option>
+                        <option value="Archery"></option>
+                        <option value="Arts"></option>
+                        <option value="Athletics"></option>
+                        <option value="Aerobics"></option>
+                        <option value="Badminton"></option>
+                        <option value="Basketball"></option>
+                        <option value="Bodybuilding"></option>
+                        <option value="Billiards"></option>
+                        <option value="Boxing"></option>
+                        <option value="Chess"></option>
+                        <option value="Cricket"></option>
+                        <option value="Fencing"></option>
+                        <option value="Football"></option>
+                        <option value="Golf"></option>
+                        <option value="Gym"></option>
+                        <option value="Hockey"></option>
+                        <option value="Kabaddi"></option>
+                        <option value="Karate"></option>
+                        <option value="Kho-Kho"></option>
+                        <option value="MMA"></option>
+                        <option value="Motorsports"></option>
+                        <option value="Rugby"></option>
+                        <option value="Shooting"></option>
+                        <option value="Skating"></option>
+                        <option value="Sports"></option>
+                        <option value="Squash"></option>
+                        <option value="Swimming"></option>
+                        <option value="Table-Tennis"></option>
+                        <option value="Taekwondo"></option>
+                        <option value="Tennis"></option>
+                        <option value="Volleyball"></option>
+                        <option value="Wrestling"></option>
+                        <option value="Yoga"></option>
                         <option value="Personal Gym Trainer"></option>
                         <option value="Fitness Training"></option>
                         <option value="Pilates"></option>
