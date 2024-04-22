cce4b6942f04e644f8369dd43b54b1cd629b5d53
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-04-22 19:18:01 +0530

    views

--- src/components/master-settings/BlogUpdate.jsx
+++ src/components/master-settings/BlogUpdate.jsx
@@ -253,7 +253,7 @@ const BlogUpdate = () => {
       },
     });
     const data = response.data.data[0];
-    setViews(response?.data?.views);
+    setViews(response?.data?.views[0]?.MatchCount);
     if (data) {
       setFormData({
         ...formData,
