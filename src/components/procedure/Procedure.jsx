568168e0102240c31b0cf4d4e91381f635360ffd
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-07-10 17:01:16 +0530

    procedure

--- src/components/procedure/Procedure.jsx
+++ src/components/procedure/Procedure.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import axios from "axios";
+import React from 'react';
+import axios from 'axios';
 import { toast } from 'react-toastify';
-import {EXECUTE_PROCEDURE, getDecryptedToken} from "../utils/Constants"
+import { EXECUTE_PROCEDURE, getDecryptedToken } from '../utils/Constants';
+
 const Procedure = () => {
     const decryptedToken = getDecryptedToken();
-    const handleSubmit1 = () => {
-        axios.post(EXECUTE_PROCEDURE, {
-            procedure: "insert_unique_coach_listings"
-        }, {
-            headers: {
-                Authorization: `Bearer ${decryptedToken}`,
-            },
+
+    const handleSubmit = (procedure, successMessage) => {
+        axios.post(EXECUTE_PROCEDURE, { procedure }, {
+            headers: { Authorization: `Bearer ${decryptedToken}` },
         })
         .then((response) => {
             if (response.data.status === 1) {
-                toast.success("Coach Procedure Successful", {
-                    position: "top-center",
+                toast.success(successMessage, {
+                    position: 'top-center',
                     autoClose: 2000,
                 });
             } else {
                 toast.error(response.data.message, {
-                    position: "top-center",
+                    position: 'top-center',
                     autoClose: 2000,
                 });
             }
@@ -28,36 +26,18 @@ const Procedure = () => {
         .catch((error) => {
             console.log(error);
         });
-    }
-    const handleSubmit2 = () => {
-        axios.post(EXECUTE_PROCEDURE, {
-            procedure: "Insert_Verified_Academies_Coaches"
-        }, {
-            headers: {
-                Authorization: `Bearer ${decryptedToken}`,
-            },
-        })
-        .then((response) => {
-            if (response.data.status === 1) {
-                toast.success("Academy Procedure Successful", {
-                    position: "top-center",
-                    autoClose: 2000,
-                });
-            } else {
-                toast.error(response.data.message, {
-                    position: "top-center",
-                    autoClose: 2000,
-                });
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }
+    };
+
+    const procedures = [
+        { id: 1, title: 'Insert unique coach listings', procedure: 'insert_unique_coach_listings', successMessage: 'Coach Procedure Successful' },
+        { id: 2, title: 'Insert Verified Academies Coaches', procedure: 'Insert_Verified_Academies_Coaches', successMessage: 'Academy Procedure Successful' },
+        { id: 3, title: 'Insert nearby unique coach listings', procedure: 'insert_nearby_unique_coach_listings', successMessage: 'Coach Listings Procedure Successful' },
+    ];
+
     return (
         <>
             <div className='service-req-top'>
-                <p className="common-fonts ss-heading ticket-head-left"> Procedure</p>
+                <p className="common-fonts ss-heading ticket-head-left">Procedure</p>
             </div>
             <div className="marketing-all-table ticket_tbl">
                 <table>
@@ -69,30 +49,25 @@ const Procedure = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr >
-                            <td className="common-fonts">1</td>
-                            <td className="common-fonts">Insert unique coach listings</td>
-                            <td className="common-fonts"><button
-                                className="common-save-button permission-save-btn"
-                            onClick={handleSubmit1}
-                            >
-                                Execute
-                            </button></td>
-                        </tr>
-                        <tr >
-                            <td className="common-fonts">2</td>
-                            <td className="common-fonts">Insert Verified Academies Coaches</td>
-                            <td className="common-fonts"><button
-                                className="common-save-button permission-save-btn"
-                            onClick={handleSubmit2}
-                            >
-                                Execute
-                            </button></td>
-                        </tr>
-                    </tbody> 
+                        {procedures.map(({ id, title, procedure, successMessage }) => (
+                            <tr key={id}>
+                                <td className="common-fonts">{id}</td>
+                                <td className="common-fonts">{title}</td>
+                                <td className="common-fonts">
+                                    <button
+                                        className="common-save-button permission-save-btn"
+                                        onClick={() => handleSubmit(procedure, successMessage)}
+                                    >
+                                        Execute
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             </div>
-        </>)
-}
+        </>
+    );
+};
 
-export default Procedure
+export default Procedure;
