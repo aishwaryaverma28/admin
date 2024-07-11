76ac739739d7ee0fc4e8b6f50bc725651eefe042
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-07-11 15:57:18 +0530

    support

--- src/components/academytickets/Support.jsx
+++ src/components/academytickets/Support.jsx
@@ -2,28 +2,31 @@ import React, { useEffect, useState } from 'react';
 import '../styles/CPGenral.css';
 import axios from 'axios';
 import {
-  FILTER_TICKETS,
+  ACADEMY_TICKETS,
   getDecryptedToken,
 } from '../utils/Constants';
-import ServiceRequestTab from '../settings/ServiceRequestTab';
 import SupportRequest from "./SupportRequest";
 
 const Support = () => {
   const decryptedToken = getDecryptedToken();
   const [ticket, setTicket] = useState([]);
   const [selectedTicket, setSelectedTicket] = useState(null);
-  const [isServiceTabOpen, setIsServiceTabOpen] = useState(false);
   const [isEditOpen, setIsEditOpen] = useState(false);
-  const [isLoading,setLoading] = useState(true);
-  const [selectedOption, setSelectedOption] = useState('0');
+  const [isLoading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const limit = 10;
+  const [pageGroup, setPageGroup] = useState(1);
+  const pagesPerGroup = 3;
 
-  const getTicket = () => {
-    const body ={
-      cond: "filter",
-      type: selectedOption,
-  }
+  const getTicket = (page) => {
+    const body = {
+      sort: "t.id desc",
+      page: page,
+      limit: limit,
+      cond: "t.title = 'contact support'",
+    }
     axios
-      .post(FILTER_TICKETS,body, {
+      .post(ACADEMY_TICKETS, body, {
         headers: {
           Authorization: `Bearer ${decryptedToken}`,
         },
@@ -39,105 +42,127 @@ const Support = () => {
   };
 
   useEffect(() => {
-    getTicket();
-  }, [selectedOption]);
+    getTicket(page);
+  }, [page]);
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const totalPages = 100;
+
+  const handleNextPage = () => {
+    if (page < totalPages) {
+      setPage((prevPage) => prevPage + 1);
+      if (page % pagesPerGroup === 0) {
+        setPageGroup((prevGroup) => prevGroup + 1);
+      }
+    }
   };
 
-  const handleOpenServiceTab = (item) => {
-    setSelectedTicket(item);
-    setIsServiceTabOpen(true);
-    
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage((prevPage) => prevPage - 1);
+      if ((page - 1) % pagesPerGroup === 0) {
+        setPageGroup((prevGroup) => prevGroup - 1);
+      }
+    }
   };
 
-  const handleCloseServiceTab = () => {
-    setIsServiceTabOpen(false);
+  const handlePageClick = (pageNumber) => {
+    setPage(pageNumber);
   };
 
+  const renderPageNumbers = () => {
+    const startPage = (pageGroup - 1) * pagesPerGroup + 1;
+    const endPage = Math.min(startPage + pagesPerGroup - 1, totalPages);
+    const pageNumbers = [];
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(
+        <button
+          key={i}
+          className={`page_number ${i === page ? "active" : ""}`}
+          onClick={() => handlePageClick(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+    return pageNumbers;
+  };
   const openContactTab = (item) => {
     setSelectedTicket(item);
     setIsEditOpen(true);
   }
   const serviceRefresh = () => {
-    getTicket();
+    getTicket(page);
   }
 
   const handleEditClose = () => {
     setIsEditOpen(false);
   }
 
-    return (
-      <div>
-        {isLoading ? (
-           <div className='support-no-ticket-found'>
-           <p className='common-fonts'>Loading...</p>
-           </div>
-        ) :
-        ticket.length === 0 ? (
-           <div className='support-no-ticket-found'>
-          <p className='common-fonts'>No ticket found</p>
+  return (
+    <div>
+      <>
+        <div className='service-req-top'>
+          <p className="common-fonts ss-heading ticket-head-left">Support</p>
+          <div className="select action-select">
           </div>
-        ) : (
-          <>
-          <div className='service-req-top'>
-            <p className="common-fonts ss-heading ticket-head-left">Support</p>
-            <div className="select action-select">
-            <select value={selectedOption} onChange={handleOptionChange} id="sports_lead">
-              <option value="0">Open</option>
-              <option value="1">Answered</option>
-              <option value="2">Closed</option>
-            </select>
-            </div>
-            <button type="button" className="helpBtn genral-refresh-icon label-refresh-icon" title="Refresh" onClick={serviceRefresh}>
-              <i class="fa-sharp fa-solid fa-rotate "></i>
-              </button>
-          </div>
-            <div className="service-support-table ticket-table">
-              <table>
-                <thead>
-                  <tr>
-                    <th className="common-fonts">S No</th>
-                    <th className="common-fonts">Title</th>
-                    <th className="common-fonts">Description</th>
-                    <th className="common-fonts">Category</th>
-                    <th className="common-fonts">Priority</th>
-                    <th className="common-fonts">Status</th>
-                    <th className="common-fonts">Created Date</th>
-                    <th className="common-fonts">Update Date</th>
-                    <th></th>
-                  </tr>
-                </thead>
-  
+          <button type="button" className="helpBtn genral-refresh-icon label-refresh-icon" title="Refresh" onClick={serviceRefresh}>
+            <i class="fa-sharp fa-solid fa-rotate "></i>
+          </button>
+        </div>
+        <div className="service-support-table ticket-table">
+          <table>
+            <thead>
+              <tr>
+                <th className="common-fonts">S No</th>
+                <th className="common-fonts">Title</th>
+                <th className="common-fonts">Description</th>
+                <th className="common-fonts">Category</th>
+                <th className="common-fonts">Status</th>
+                <th className="common-fonts">Created Date</th>
+              </tr>
+            </thead>
+            {isLoading ? (
+              <div className='support-no-ticket-found'>
+                <p className='common-fonts'>Loading...</p>
+              </div>
+            ) :
+              ticket.length === 0 ? (
+                <div className='support-no-ticket-found'>
+                  <p className='common-fonts'>No ticket found</p>
+                </div>
+              ) : (
                 <tbody>
                   {ticket.map((item) => (
-                    <tr key={item.id} >
+                    <tr key={item.id} onClick={() => openContactTab(item)}>
                       <td className="common-fonts">{item.id}</td>
-                      <td className="common-fonts" onClick={() => handleOpenServiceTab(item)} >{item.title} </td>
-                      <td className="common-fonts" onClick={() => handleOpenServiceTab(item)} ><div className="leads_desc">{item.description}</div></td>
-                      <td className="common-fonts" onClick={() => handleOpenServiceTab(item)}>{item.category}</td>
-                      <td className="common-fonts" onClick={() => handleOpenServiceTab(item)}>{item.priority}</td>
-                      <td className="common-fonts" onClick={() => handleOpenServiceTab(item)}>{item.status}</td>
-                      <td className="common-fonts" onClick={() => handleOpenServiceTab(item)}>{item.created_at.split('T')[0]}</td>
-                      <td className="common-fonts" onClick={() => handleOpenServiceTab(item)}>{item.updated_at.split('T')[0]}</td>
-                      <td onClick={() => openContactTab(item)}><div><i className="fa-solid fa-pen"></i></div></td>
+                      <td className="common-fonts" >{item.title} </td>
+                      <td className="common-fonts" ><div className="leads_desc">{item.description}</div></td>
+                      <td className="common-fonts">{item.category}</td>
+                      <td className="common-fonts">{item.status}</td>
+                      <td className="common-fonts">{item.created_at.split('T')[0]}</td>
                     </tr>
                   ))}
                 </tbody>
-              </table>
-            </div>
-          </>
-        )}
-        {isServiceTabOpen && <ServiceRequestTab ticket={selectedTicket} onClose={handleCloseServiceTab} />}
+              )}
+          </table>
+          <div className="table_pagination">
+            <button onClick={handlePreviousPage} disabled={page === 1}>
+              &lt;
+            </button>
+            {renderPageNumbers()}
+            <button onClick={handleNextPage} disabled={page === totalPages}>
+              &gt;
+            </button>
+          </div>
+        </div>
+      </>
+      {
+        isEditOpen && <SupportRequest onClose={handleEditClose} ticket={selectedTicket} getTicket={getTicket} page={page}/>
+      }
+    </div>
+  );
 
-        {
-          isEditOpen && <SupportRequest onClose={handleEditClose} ticket={selectedTicket}/>
-        }
-      </div>
-    );
-
-  }
+}
 
 
 
--- src/components/academytickets/SupportRequest.jsx
+++ src/components/academytickets/SupportRequest.jsx
@@ -1,33 +1,33 @@
 import React, { useRef, useState } from "react";
 import "../styles/CPGenral.css";
 import axios from "axios";
-import { REPLY_TICKET, getDecryptedToken } from "../utils/Constants";
+import { cdnurl,ADD_TICKET_REPLY, getDecryptedToken, config } from "../utils/Constants";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-const SupportRequest = ({ onClose, ticket }) => {
+import AWS from 'aws-sdk';
+const SupportRequest = ({ onClose, ticket, getTicket, page }) => {
+  window.Buffer = window.Buffer || require("buffer").Buffer;
   const decryptedToken = getDecryptedToken();
   const fileInputRef = useRef(null);
   const [fileName, setFileName] = useState("");
   const [stateBtn, setStateBtn] = useState(0);
   const [details, setDetails] = useState({
-    ticketId: ticket?.id,
-    reply:""
+    status: "",
+    description: "",
+    attachment: "",
+    parent_id: ticket?.id,
   });
+  const processImageName = (imageName) => {
+    const nameParts = imageName.split(".");
+    if (nameParts?.length > 1) {
+        const namePart = nameParts.slice(0, -1).join(".");
+        const processedName = namePart.replace(/[^\w-]/g, "-");
+        return `${processedName}.${nameParts[nameParts.length - 1]}`;
+    } else {
+        return imageName.replace(/[^\w-]/g, "-");
+    }
+};
 
-
-// parent_id
-// : 
-// 1
-// query
-// : 
-// null
-// status
-// : 
-// null
-// user_id
-// : 
-// null
   const handleBrowseClick = () => {
     fileInputRef.current.click();
   };
@@ -39,44 +39,76 @@ const SupportRequest = ({ onClose, ticket }) => {
     });
     setStateBtn(1);
   }
-
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      setFileName(file.name);
+      submitImage(file);
     }
   };
+  const submitImage = (file) => {
+    const selectedImage = file;
 
+    if (selectedImage) {
+        const processedFileName = processImageName(selectedImage.name);
+        const modifiedFile = new File([selectedImage], processedFileName, { type: selectedImage.type });
+
+        AWS.config.update({
+            accessKeyId: config.accessKeyId,
+            secretAccessKey: config.secretAccessKey,
+            region: config.region,
+        });
+
+        const s3 = new AWS.S3();
+        const params = {
+            Bucket: 'destcdn90',
+            Body: modifiedFile,
+            Key: `attachments/tickets/${modifiedFile.name}`,
+        };
+
+        s3.upload(params, (err, data) => {
+            if (err) {
+                console.error('Error uploading file:', err);
+            } else {
+                setFileName(modifiedFile.name);
+                setDetails((prevDetails) => ({
+                    ...prevDetails,
+                    attachment: modifiedFile.name,
+                }));
+            }
+        });
+    }
+};
   const handleUpdate = (event) => {
     event.preventDefault();
     console.log(details);
     axios
-      .post(REPLY_TICKET, details, {
+      .post(ADD_TICKET_REPLY, details, {
         headers: {
           Authorization: `Bearer ${decryptedToken}`,
         },
       })
       .then((response) => {
-        console.log(response);
-        if (response?.data?.status === 1){
-        toast.success("Reply is added successfully", {
-          position: "top-center",
-          autoClose: 2000,
-        });
-      }else{
-        toast.error(response?.data?.message, {
-          position: "top-center",
-          autoClose: 1000,
-        });
-      }
+        if (response?.data?.status === 1) {
+          toast.success("Reply is added successfully", {
+            position: "top-center",
+            autoClose: 2000,
+          });
+        } else {
+          toast.error(response?.data?.message, {
+            position: "top-center",
+            autoClose: 1000,
+          });
+        }
+        getTicket(page);
       })
       .catch((error) => {
-console.log(error)
+        console.log(error)
       })
   };
   return (
     <div className="popup-wrapper">
       <div className="product-popup-container">
+      <div className="leftCreateClose" onClick={onClose}></div>
         <div className="product-popup-box edit-service-box">
           <div>
             <p className="common-fonts contact-support-heading">Edit Service</p>
@@ -84,14 +116,14 @@ console.log(error)
               <form>
                 <div className="contact-tab-fields">
                   <label htmlFor="" className="common-fonts contact-tab-label">
-                    Title
+                    Name
                   </label>
                   <input
                     type="text"
-                    name="title"
+                    name="name"
                     onChange={handleChange}
                     className="common-fonts common-input contact-tab-input"
-                    value={ticket?.title}
+                    value={ticket?.name}
                     disabled
                   />
                 </div>
@@ -117,10 +149,10 @@ console.log(error)
                   <div>
                     <input
                       type="text"
-                      name="mobile"
+                      name="phone"
                       onChange={handleChange}
                       className="common-input contact-tab-input"
-                      value={ticket?.mobile}
+                      value={ticket?.phone}
                       disabled
                     />
                   </div>
@@ -142,34 +174,20 @@ console.log(error)
 
                 <div className="contact-tab-fields">
                   <label htmlFor="" className="common-fonts contact-tab-label">
-                    Type of issue
+                    Status
                   </label>
                   <select
-                    name="category"
-                    onChange={handleChange}
                     className="common-input contact-type-of-issue"
-                    value={ticket?.category}
-                    disabled
-                  >
-                    <option value="technical">Technical</option>
-                    <option value="Non Technical">Non Technical</option>
-                  </select>
-                </div>
-
-                <div className="contact-tab-fields">
-                  <label htmlFor="" className="common-fonts contact-tab-label">
-                    Priority
-                  </label>
-                  <select
-                    name="priority"
+                    value={ticket?.status}
                     onChange={handleChange}
-                    className="common-input contact-type-of-issue"
-                    value={ticket?.priority}
-                    disabled
+                    name="status"
                   >
-                    <option value="Low">Low</option>
-                    <option value="Average">Average</option>
-                    <option value="High">High</option>
+                    <option value=""></option>
+                    <option value="waiting for support">waiting for support</option>
+                    <option value="in progress">in progress</option>
+                    <option value="canceled">canceled</option>
+                    <option value="resolved">resolved</option>
+                    <option value="closed">closed</option>
                   </select>
                 </div>
 
@@ -178,14 +196,14 @@ console.log(error)
                     Reply
                   </label>
                   <textarea
-                    name="reply"
+                    name="description"
                     onChange={handleChange}
                     className="common-fonts common-input contact-tab-input contact-tab-textarea"
                     placeholder="Reply to the issue raised"
-                    value={details?.reply}
+                    value={details?.description}
                   ></textarea>
                 </div>
-                {/* <div className="contact-tab-fields">
+                <div className="contact-tab-fields">
                   <label
                     htmlFor="fileInput"
                     className="common-fonts contact-tab-label"
@@ -230,7 +248,7 @@ console.log(error)
                       )}
                     </span>
                   </div>
-                </div> */}
+                </div>
                 <div className="contact-support-button">
                   {stateBtn === 0 ? (
                     <button className="disabledBtn" disabled>
