c1eb3183c2f38b06e7d02c87cde0e0516ab65721
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-07-21 15:44:41 +0530

    css update

--- src/components/acadmey/MailerControl.jsx
+++ src/components/acadmey/MailerControl.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MailerControl = () => {
+    const [stateBtn, setStateBtn] = useState(0);
     return (
-        <>
+        <section className='mailer-sec'>
             <p className='common-fonts reply-head'>
                 Mailer Control
             </p>
             <div className='flexBox mailer-conatiner'>
                 <div className='box1 flexBox'>
                     <p className='common-fonts'>Status: </p>
-                    <label className="password-switch">
+                    <label className="password-switch lead-switch">
                         <input
                             type="checkbox"
                         />
-                        <span className="password-slider password-round"></span>
+                        <span className="password-slider lead-slider password-round"></span>
                     </label>
                 </div>
                 <div className='box2 flexBox'>
-                    <p className='common-fonts'>Start Date: </p>
+                    <p className='common-fonts p-width'>Start Date: </p>
                     <input className='editInput'
                         type="date"
                         name="start_date"
                     />
                 </div>
                 <div className='box2 flexBox'>
-                    <p className='common-fonts'>End Date: </p>
+                    <p className='common-fonts p-width'>End Date: </p>
                     <input className='editInput'
                         type="date"
                         name="end_date"
@@ -32,7 +33,7 @@ const MailerControl = () => {
                 </div>
             </div>
             <div className='flexBox mailer-conatiner'>
-                <div className='box2 flexBox'>
+                <div className='box3 flexBox'>
                     <p className='common-fonts'>Frequency : </p>
                     <span className='newEditableArea'>
                         <select
@@ -51,13 +52,24 @@ const MailerControl = () => {
                         />
                     </span>
                 </div>
+                {stateBtn === 0 ? (
+                    <button className="disabledBtn new-disable" disabled>
+                        Update
+                    </button>
+                ) : (
+                    <button
+                        className="common-save-button permission-save-btn"
+                    >
+                        Update
+                    </button>
+                 )}
                 <p className="common-fonts reply-head">
                     <button className="convertToDeal">
                         Immediate Mailer
                     </button>
                 </p>
             </div>
-        </>
+        </section>
     )
 }
 
