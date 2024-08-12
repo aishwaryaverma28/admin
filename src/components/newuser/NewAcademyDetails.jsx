dd42faf5157abc4014aedf3f037988d247d4c872
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-08-12 11:34:59 +0530

    email

--- src/components/newuser/NewAcademyDetails.jsx
+++ src/components/newuser/NewAcademyDetails.jsx
@@ -5,17 +5,21 @@ import {
     GET_ACADEMY,
     getDecryptedToken,
     UPDATE_ACADEMY,
-    ALL_SPORTS
+    ALL_SPORTS, EMAIL_VERIFY
 } from "./../utils/Constants";
-import { normalStylingInput, editStylingInput, editStylingTextarea, normalStylingTextarea, editStylingSelect1, normalStylingSelect1 } from "./../utils/variables";
+import { normalStylingInput, editStylingInput, editStylingSelect1, normalStylingSelect1 } from "./../utils/variables";
 import { toast } from "react-toastify";
 import '../styles/Comment.css'
 import QuillEditor from '../QuillEditor';
+import tick from "../../assets/image/star_tick.svg"
+import cross from "../../assets/image/unverified.svg"
+
 const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
     const decryptedToken = getDecryptedToken();
     const [keywords, setKeywords] = useState([
         "4r5e", "5h1t", "5hit", "a55", "anal", "anus", "ar5e", "arrse", "arse", "ass", "ass-fucker", "asses", "assfucker", "assfukka", "asshole", "assholes", "asswhole", "a_s_s", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", "balls", "ballsack", "bastard", "beastial", "beastiality", "bellend", "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob", "blowjobs", "boiolas", "bollock", "bollok", "boner", "boob", "boobs", "booobs", "boooobs", "booooobs", "booooooobs", "breasts", "buceta", "bugger", "bum", "bunny fucker", "butt", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", "cockface", "cockhead", "cockmunch", "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", "cocksucking", "cocksucks", "cocksuka", "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", "cum", "cummer", "cumming", "cums", "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", "cuntlicker", "cuntlicking", "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", "cyberfuckers", "cyberfucking", "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", "dlck", "dog-fucker", "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", "ejaculates", "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", "fingerfuck", "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", "fistfucked", "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", "fooker", "fuck", "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", "fuckings", "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", "gangbang", "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", "goddamn", "goddamned", "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "horniest", "horny", "hotsex", "jack-off", "jackoff", "jap", "jerk-off", "jism", "jiz", "jizm", "jizz", "kawk", "knob", "knobead", "knobed", "knobend", "knobhead", "knobjocky", "knobjokey", "kock", "kondum", "kondums", "kum", "kummer", "kumming", "kums", "kunilingus", "l3i+ch", "l3itch", "labia", "lust", "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "masterb8", "masterbat*", "masterbat3", "masterbate", "masterbation", "masterbations", "masturbate", "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas", "mothafuckaz", "mothafucked", "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", "mothafucks", "mother fucker", "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", "motherfucking", "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", "muthafuckker", "muther", "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah", "niggas", "niggaz", "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", "nutsack", "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", "phonesex", "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", "piss", "pissed", "pisser", "pissers", "pisses", "pissflaps", "pissin", "pissing", "pissoff", "poop", "porn", "porno", "pornography", "pornos", "prick", "pricks", "pron", "pube", "pusse", "pussi", "pussies", "pussy", "pussys", "rectum", "retard", "rimjaw", "rimming", "s hit", "s.o.b.", "sadist", "schlong", "screwing", "scroat", "scrote", "scrotum", "semen", "sex", "sh!+", "sh!t", "sh1t", "shag", "shagger", "shaggin", "shagging", "shemale", "shi+", "shit", "shitdick", "shite", "shited", "shitey", "shitfuck", "shitfull", "shithead", "shiting", "shitings", "shits", "shitted", "shitter", "shitters", "shitting", "shittings", "shitty", "skank", "slut", "sluts", "smegma", "smut", "snatch", "son-of-a-bitch", "spac", "spunk", "s_h_i_t", "t1tt1e5", "t1tties", "teets", "teez", "testical", "testicle", "tit", "titfuck", "tits", "titt", "tittie5", "tittiefucker", "titties", "tittyfuck", "tittywank", "titwank", "tosser", "turd", "tw4t", "twat", "twathead", "twatty", "twunt", "twunter", "v14gra", "v1gra", "vagina", "viagra", "vulva", "w00se", "wang", "wank", "wanker", "wanky", "whoar", "whore", "willies", "willy", "xrated", "xxx"
     ]);
+    const [emailVeri, setEmailVeri] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [editedItem, setEditedItem] = useState({});
     const [stateBtn, setStateBtn] = useState(0);
@@ -121,11 +125,11 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
     const handleCitySelect = (sport) => {
         setSearchCity(sport?.city);
         setEditedItem(prevState => ({
-          ...prevState,
-          loc_id: sport?.id,
-          city_id: sport?.city_id,
-          location_state: sport?.state,
-          city: sport?.city + ", " + sport?.state + " (" + sport?.type + ")",
+            ...prevState,
+            loc_id: sport?.id,
+            city_id: sport?.city_id,
+            location_state: sport?.state,
+            city: sport?.city + ", " + sport?.state + " (" + sport?.type + ")",
         }));
         setFilteredCity([]);
         setIsCityDropdownVisible(false);
@@ -138,11 +142,11 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
             } else if (filteredCity.length > 0) {
                 setSearchCity(filteredCity[0]?.city);
                 setEditedItem(prevState => ({
-                  ...prevState,
-                  loc_id: filteredCity[0]?.id,
-                  city_id: filteredCity[0]?.city_id,
-                  location_state: filteredCity[0]?.state,
-                  city: filteredCity[0]?.city + ", " + filteredCity[0]?.state + " (" + filteredCity[0]?.type + ")",
+                    ...prevState,
+                    loc_id: filteredCity[0]?.id,
+                    city_id: filteredCity[0]?.city_id,
+                    location_state: filteredCity[0]?.state,
+                    city: filteredCity[0]?.city + ", " + filteredCity[0]?.state + " (" + filteredCity[0]?.type + ")",
                 }));
             }
             setIsCityDropdownVisible(false);
@@ -177,13 +181,16 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
                     setTrainingLocation(trainingLocationArray);
                 }
                 if (response?.data?.data[0]?.sport_id) {
-                  setSearchTerm(response?.data?.data[0]?.sport_name)
+                    setSearchTerm(response?.data?.data[0]?.sport_name)
                 }
                 if (response?.data?.data[0]?.location_locality) {
                     setSearchCity(response?.data?.data[0]?.location_locality)
-                }else if (response?.data?.data[0]?.location_city) {
+                } else if (response?.data?.data[0]?.location_city) {
                     setSearchCity(response?.data?.data[0]?.location_city)
-                  }
+                }
+                if (response?.data?.data[0]?.email) {
+                    checkEmail(response?.data?.data[0]?.email);
+                }
                 setIsLoading(false);
             })
             .catch((error) => {
@@ -191,6 +198,22 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
                 setIsLoading(false);
             });
     };
+    function checkEmail(email) {
+        axios.post(EMAIL_VERIFY, { email: email }, {
+            headers: {
+                Authorization: `Bearer ${decryptedToken}`,
+            },
+        })
+            .then((response) => {
+                console.log(response?.data?.status);
+                if (response?.data?.status === 1) {
+                    setEmailVeri(true);
+                }
+            })
+            .catch((error) => [
+                console.log(error)
+            ])
+    }
     const fetchSports = () => {
         let body = {
             sort: "name asc"
@@ -208,13 +231,13 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
                 console.log(error);
             });
     }
-    useEffect(() => {    
+    useEffect(() => {
         fetchSports();
-      }, []);
-      useEffect(() => {    
+    }, []);
+    useEffect(() => {
         fetchLead();
-      }, [sports]);
-      
+    }, [sports]);
+
     const capitalizeFirstLetterOfEachWord = (string) => {
         return string?.replace(/\b\w/g, char => char?.toUpperCase());
     };
@@ -270,13 +293,13 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
     };
     const handleDataTransfer = (data) => {
         if (!isDisabled) {
-          setEditedItem({
-            ...editedItem,
-            about: data,
-          });
+            setEditedItem({
+                ...editedItem,
+                about: data,
+            });
         }
         setStateBtn(1);
-      };
+    };
 
     const handleCheckboxChange = (event) => {
         const value = event.target.value;
@@ -362,13 +385,13 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
         handleUpdateClick
     }));
 
-   return (
+    return (
         <>
             <div className="user-details--left">
                 <div className="user-details--heading">
                     <div className="user-details-imgBox">
                         <img
-                             src={editedItem?.logo === null || editedItem?.logo === ""
+                            src={editedItem?.logo === null || editedItem?.logo === ""
                                 ? `${cdnurl}asset/images/logo.svg`
                                 : `${cdnurl}academy_temp/${editedItem?.id}/${editedItem?.logo}`}
                             alt="logo"
@@ -380,7 +403,7 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
                                     <span>-</span>
                                 ) : (
                                     <>
-                                       {editedItem?.id}: {editedItem?.name}, {editedItem?.location_locality}
+                                        {editedItem?.id}: {editedItem?.name}, {editedItem?.location_locality}
                                     </>
                                 )}
                             </p>
@@ -502,7 +525,9 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
                                                     onChange={handleInputChange}
                                                     checked={editedItem?.email_verified === 1}
                                                 /> Email Verified
-
+                                                <div className="mail">
+                                                    {emailVeri ? <img src={tick} alt="verified" className='img_size_1' /> : <img src={cross} alt="unverified" className='img_size_1' />}
+                                                </div>
                                             </label>
                                         </span>
                                     )}
@@ -758,14 +783,14 @@ const NewAcademyDetails = React.forwardRef(({ id, updateCheckState }, ref) => {
                                         <span>-</span>
                                     ) : (
                                         <span>
-                                        <div className={`notesEditor ${isEditable ? 'details' : 'editDetails'}`}>
-                                          <QuillEditor
-                                            onDataTransfer={handleDataTransfer}
-                                            initialContent={editedItem?.about}
-                                            readOnly={isDisabled}
-                                          />
-                                        </div>
-                                      </span>
+                                            <div className={`notesEditor ${isEditable ? 'details' : 'editDetails'}`}>
+                                                <QuillEditor
+                                                    onDataTransfer={handleDataTransfer}
+                                                    initialContent={editedItem?.about}
+                                                    readOnly={isDisabled}
+                                                />
+                                            </div>
+                                        </span>
                                     )}
                                 </p>
                             </div>
